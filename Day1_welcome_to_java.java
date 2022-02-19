eb34ed6e0da90e11c5f872a6aeafa82e22d8ed95
Mevnz <ivanmuliansyah@gmail.com>
2022-02-19 19:57:39 +0700

    algoritma dan struktur data

---
+++ Day1_welcome_to_java.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day1_welcome_to_java;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Day1_welcome_to_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+    
+}
