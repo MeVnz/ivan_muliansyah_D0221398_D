0909659991d8e64cce99e5558b03a967d6d6682d
MeVnz <ivanmuliansyah@gmail.com>
2022-02-20 20:59:17 +0700

    algoritma dan struktur data

---
+++ Day2_java_stdin_and_stdout_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day2_java_stdin_and_stdout_1;
+import java.util.*;
+/**
+ *
+ * @author Ivan
+ */
+public class Day2_java_stdin_and_stdout_1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
