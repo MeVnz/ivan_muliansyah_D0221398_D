b4414bddd73d8fd83fb7edb1e7e4281c41e04c2a
MeVnz <ivanmuliansyah@gmail.com>
2022-02-22 15:41:22 +0700

    algoritma dan struktur data

---
+++ Day4_java_stdin_and_stdout_2.java
@@ -0,0 +1,20 @@
+package day4_java_stdin_and_stdout_2;
+
+import java.util.Scanner;
+
+public class Day4_java_stdin_and_stdout_2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+    
+}
