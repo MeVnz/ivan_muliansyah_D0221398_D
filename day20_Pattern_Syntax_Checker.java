e18c9635422a81c9babac2190f45541249062ac0
MeVnz <ivanmuliansyah@gmail.com>
2022-03-11 20:05:34 +0700

    algoritma dan struktur data

---
+++ day20_Pattern_Syntax_Checker.java
@@ -0,0 +1,22 @@
+package PatternSyntaxChecker;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+
+public class day20_Pattern_Syntax_Checker {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+            try{
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            }catch(PatternSyntaxException ex){
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+    }
+}
